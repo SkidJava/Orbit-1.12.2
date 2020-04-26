9b360fa948704d6c3f24c1321a1e43dd91e3d5cf
SkidJava <skidjava@gmail.com>
2020-04-27 02:27:46 +0900

    Fix AutoArmor

--- src/main/java/client/module/combat/AutoArmor.java
+++ src/main/java/client/module/combat/AutoArmor.java
@@ -1,188 +1,124 @@
 package client.module.combat;
 
+import client.Client;
 import client.event.EventTarget;
 import client.event.events.update.EventUpdate;
 import client.module.Module;
 import client.setting.Setting;
 import client.utils.ArmorUtils;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.ClickType;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import org.lwjgl.input.Keyboard;
 
 public class AutoArmor extends Module {
-    public AutoArmor() {
-        super("AutoArmor", Keyboard.KEY_NONE, Category.COMBAT);
-        managers.settingManager.addSetting(new Setting(this, "Best", true));
+
+    private enum ArmorType {
+
+        BOOTS(0, 8),
+        LEGS(1, 7),
+        CHEST(2, 6),
+        HELMET(3, 5);
+
+        ArmorType(int id, int slotID) {
+            this.id = id;
+            this.slotID = slotID;
+        }
+
+        private int id;
+        private int slotID;
+
+        public int getID() {
+            return id;
+        }
+        
+        public int getSlotID() {
+            return slotID;
+        }
     }
 
-    // FIXME: WindowClickのメゾットで使ってるClickTypeがだめなのか, わからない
+    private int[] chest;
+    private int[] legs;
+    private int[] boots;
+    private int[] helmet;
+    private ArmorType currentTarget = ArmorType.BOOTS;
+
+    public AutoArmor() {
+        super("AutoArmor", Keyboard.KEY_NONE, Category.COMBAT);
+
+        this.chest = new int[]{311, 307, 315, 303, 299};
+        this.legs = new int[]{312, 308, 316, 304, 300};
+        this.boots = new int[]{313, 309, 317, 305, 301};
+        this.helmet = new int[]{310, 306, 314, 302, 298};
+    }
+
     @EventTarget
     public void onEnable() {
         super.onEnable();
-        this.chestplate = new int[]{311, 307, 315, 303, 299};
-        this.leggings = new int[]{312, 308, 316, 304, 300};
-        this.boots = new int[]{313, 309, 317, 305, 301};
-        this.helmet = new int[]{310, 306, 314, 302, 298};
-        this.delay = 0;
     }
 
+    //0 = boots, 1 = legs, 2 = chest, 3 = Helmet
     @EventTarget
     public void onUpdate(EventUpdate event) {
-        if (managers.settingManager.getSetting(this, "Best").getBooleanValue()) {
-            betterArmor();
-        } else {
-            autoArmor();
-        }
-    }
-    public void autoArmor() {
-        if (managers.settingManager.getSetting(this, "Best").getBooleanValue()) {
-            return;
-        }
-        int item = -1;
-        this.delay += 1;
-        if (this.delay >= 10) {
-            if (mc.thePlayer.inventory.armorInventory.get(0) == null) {
-                int[] boots;
-                int length = (boots = this.boots).length;
-                for (int i = 0; i < length; i++) {
-                    int id = boots[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
+        //System.out.println(mc.thePlayer.inventory.armorInventory.get(ArmorType.BOOTS.getID()).getEnchantmentTagList());
+
+        int bestArmor = -1;
+
+        for (int i = 0; i < 45; i++) {
+            ItemStack item = mc.thePlayer.inventoryContainer.getSlot(i).getStack();
+            if (Item.getIdFromItem(item.getItem()) == 0) continue;
+
+            if(isArmor(Item.getIdFromItem(item.getItem()))) {
+                if (bestArmor == -1) {
+                    bestArmor = i;
+                    continue;
                 }
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(1) == null) {
-                int[] leggings;
-                int length = (leggings = this.leggings).length;
-                for (int i = 0; i < length; i++) {
-                    int id = leggings[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(2) == null) {
-                int[] chestplate;
-                int length = (chestplate = this.chestplate).length;
-                for (int i = 0; i < length; i++) {
-                    int id = chestplate[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(3) == null) {
-                int[] helmet;
-                int length = (helmet = this.helmet).length;
-                for (int i = 0; i < length; i++) {
-                    int id = helmet[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (item != -1) {
-                mc.playerController.windowClick(0, item, 0, ClickType.QUICK_MOVE, mc.thePlayer);
-                this.delay = 0;
+
+                ItemStack bestItem = mc.thePlayer.inventoryContainer.getSlot(bestArmor).getStack();
+                int bestScore = getArmorScore(Item.getIdFromItem(bestItem.getItem()));
+                int tempScore = getArmorScore(Item.getIdFromItem(item.getItem()));
+
+                if (tempScore > bestScore) bestArmor = i;
             }
         }
+
+        if(bestArmor > 8) {
+            ItemStack item = mc.thePlayer.inventory.armorInventory.get(currentTarget.getID());
+            if(Item.getIdFromItem(item.getItem()) != 0) mc.playerController.windowClick(0, currentTarget.getSlotID(), 0, ClickType.QUICK_MOVE, mc.thePlayer);
+            else {
+                mc.playerController.windowClick(0, bestArmor, 0, ClickType.QUICK_MOVE, mc.thePlayer);
+                currentTarget = getArmorType(currentTarget.getID() + 1 % 4);
+            }
+        } else currentTarget = getArmorType((currentTarget.getID() + 1) % 4);
     }
 
-    public void betterArmor() {
-        if (!managers.settingManager.getSetting(this, "Best").getBooleanValue()) {
-            return;
-        }
-        this.delay += 1;
-        if ((this.delay >= 10) && ((mc.thePlayer.openContainer == null) || (mc.thePlayer.openContainer.windowId == 0))) {
-            boolean switchArmor = false;
-            int item = -1;
-            if (mc.thePlayer.inventory.armorInventory.get(0) == null) {
-                int[] array;
-                int j = (array = this.boots).length;
-                for (int i = 0; i < j; i++) {
-                    int id = array[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (ArmorUtils.isBetterArmor(0, this.boots)) {
-                item = 8;
-                switchArmor = true;
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(3) == null) {
-                int[] array;
-                int j = (array = this.helmet).length;
-                for (int i = 0; i < j; i++) {
-                    int id = array[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (ArmorUtils.isBetterArmor(3, this.helmet)) {
-                item = 5;
-                switchArmor = true;
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(1) == null) {
-                int[] array;
-                int j = (array = this.leggings).length;
-                for (int i = 0; i < j; i++) {
-                    int id = array[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (ArmorUtils.isBetterArmor(1, this.leggings)) {
-                item = 7;
-                switchArmor = true;
-            }
-            if (mc.thePlayer.inventory.armorInventory.get(2) == null) {
-                int[] array;
-                int j = (array = this.chestplate).length;
-                for (int i = 0; i < j; i++) {
-                    int id = array[i];
-                    if (ArmorUtils.getItem(id) != -1) {
-                        item = ArmorUtils.getItem(id);
-                        break;
-                    }
-                }
-            }
-            if (ArmorUtils.isBetterArmor(2, this.chestplate)) {
-                item = 6;
-                switchArmor = true;
-            }
-            boolean b = false;
-            NonNullList<ItemStack> stackArray;
-            int k = (stackArray = mc.thePlayer.inventory.mainInventory).size();
-            for (int j = 0; j < k; j++) {
-                ItemStack stack = stackArray.get(j);
-                if (stack == null) {
-                    b = true;
-                    break;
-                }
-            }
-            switchArmor = (switchArmor) && (!b);
-            if (item != -1) {
-                // switchArmor ? 4 : 1
-                mc.playerController.windowClick(0, item, 0, switchArmor ? ClickType.PICKUP : ClickType.QUICK_MOVE, mc.thePlayer);
-                this.delay = 0;
-            }
-        }
+    public ArmorType getArmorType(int id) {
+        if(id == 0) return ArmorType.BOOTS;
+        else if(id == 1) return  ArmorType.LEGS;
+        else if(id == 2) return ArmorType.CHEST;
+        else return ArmorType.HELMET;
     }
 
-    private int[] chestplate;
-    private int[] leggings;
-    private int[] boots;
-    private int[] helmet;
-    private int delay;
+    private int getArmorScore(int id) {
+        if(id < 301) return 0;
+        else if(id <= 305) return 1;
+        else if(id <= 309) return 2;
+        else if(id <= 313) return 4;
+        else return 3;
+    }
+
+    private boolean isArmor(int id) {
+        if(currentTarget == ArmorType.BOOTS && (id == boots[0] || id == boots[1] || id == boots[2] || id == boots[3] || id == boots[4]))
+            return true;
+        else if(currentTarget == ArmorType.LEGS &&(id == legs[0] || id == legs[1] || id == legs[2] || id == legs[3] || id == legs[4]))
+            return true;
+        else if(currentTarget == ArmorType.CHEST && (id == chest[0] || id == chest[1] || id == chest[2] || id == chest[3] || id == chest[4]))
+            return true;
+        else if(currentTarget == ArmorType.HELMET && (id == helmet[0] || id == helmet[1] || id == helmet[2] || id == helmet[3] || id == helmet[4]))
+            return true;
+        else return false;
+    }
 }
